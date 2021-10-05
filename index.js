d625efae0fe98a024549a284ff5eb80c64bc0a92
Francisco <frankmartinez4050@gmail.com>
2021-10-04 19:41:12 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  return word === word.split("").reverse().join("")
 }
 
+isPalindrome('abba') // retruns true 
+isPalindrome('racecar') // returns true
+isPalindrome('a') // returns true
+isPalindrome('robot') // returns false
+isPalindrome('ab') // returns false
 /* 
-  Add your pseudocode here
+  set the word = to itself then divide it into subststring and call
+  .reverse() on it to see if the word is still equal to itself when reverse 
+  and then join it again
 */
 
 /*
-  Add written explanation of your solution here
+ set the word = to itself then divide it into subststring and call
+ .reverse() on it to see if the word is still equal to itself when reverse 
+  and then join it again
+
+  after call the function and in the parentheses put in the word you want to check
 */
 
 // You can run `node index.js` to view these console logs
